c4f24c614e7acc31784e5f5f96a471f3f3545117
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-03-18 18:33:07 +0530

    redy for deployment

--- backend/main.py
+++ backend/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://attenify.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
